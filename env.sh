ba0182fc0b3ff6e6a7500ebfbd9f381876b554e8
yhori <yuji.hori@gmail.com>
2013-05-30 12:55:33 +0900

    change env.sh

--- env.sh
+++ env.sh
@@ -7,27 +7,34 @@ vimbk=~/.backup
 
 
 dots=~/dotfiles
-manual_dir=$dot/vimfiles/refs
+manual_dir=$dots/vimfiles/refs
 
 manual_php=$manual_dir/php_manual_ja.tar.gz
 
 # create env
 echo backup dotfiles
-
-[ -d $vimrc ] && sudo mv $vimrc $vimrc.bak || exit -1
-[ -d $vimdir ] && sudo mv $vimdir ${vimdir}_bak || exit -2
-
-echo create symbolic link to dotfiles
-ln -s $dots/vimfiles $vimdir
+# 1 as already exists
+[ -f $vimrc ] && sudo mv $vimrc $vimrc.bak 
 ln -s $dots/_vimrc $vimrc
 
+# 1 as already exists
+[ -d $vimdir ] && sudo mv $vimdir ${vimdir}_bak 
+ln -s $dots/vimfiles $vimdir
+
 echo make direcotry for vim backup
-[ -d $vimbk ] && sudo mv $vimbk $vimbk.bak || sudo mkdir $vimbk
+[ -d $vimbk ] && sudo mv $vimbk $vimbk.bak 
+sudo mkdir $vimbk
+
+echo $manual_dir/php_manual_ja.tar
 
 echo Prepare php manual. refs by vim.
-[ -d $manual_dir/php_manual_ja ] && sudo tar -zxf $manual_php -C $manual_dir
+[ -d $manual_dir/php_manual_ja ] || sudo tar -zxf $manual_php -C $manual_dir
 sudo chmod 644 $manual_dir
 
-exit 
+own=`whoami`
+
+sudo chown -R $own:$own $dots 
+
+exit
 
 
